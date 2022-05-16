6ab44214d52d23c0df62f531f67dbe6853e991ce
ismaelgomesdasilva <ismaelgomesdasilva003@gmail.com>
2022-05-16 20:37:23 -0300

    style: responsividade

--- src/Pages/Home/styles.js
+++ src/Pages/Home/styles.js
@@ -6,6 +6,7 @@ export const Container = styled.div`
   margin: 7rem auto;
   display: flex;
   flex-direction: column;
+
   align-items: center;
   font-family: "Roboto";
   h3 {
@@ -45,6 +46,31 @@ export const Container = styled.div`
       transform: translate(-4%, -4%) rotate(-30deg);
     }
   }
+  @media(max-width: 900px) {
+   width: auto;
+  }
+  @media(max-width: 800px) {
+   >P{
+       width: 500px;
+       text-align: center;
+   }
+   @media(max-width: 800px) {
+   h3{
+       width: 350px;
+       text-align: center;
+   }
+   h1{
+       text-align: center;
+   }
+   >P{
+       width: 350px;
+       margin-top: 10px;
+   }
+  > div{
+    width: 350px;
+   }
+  }
+}
 `;
 export const Award = styled.div`
   width: 416px;
@@ -53,14 +79,20 @@ export const Award = styled.div`
   border-radius: 10px;
   text-align: center;
   display: flex;
-
+  padding: 20px;
+  flex-wrap: wrap;
   align-items: center;
-  img{
-      margin-left: 2rem;
+  transition: all 0.5s ease-in-out;
+  &:hover {
+    background-color: #70663a;
+    transform: scale(1.1);
   }
-div{
+  img {
+    margin-left: 2rem;
+  }
+  div {
     margin: auto;
-}
+  }
   p {
     font-weight: 700;
     font-size: 32px;
@@ -75,11 +107,25 @@ div{
     font-family: "Poppins";
     color: #ffffff;
   }
+  @media(max-width: 800px) {
+   width: 350px;
+  }
 `;
 export const AwardDiv = styled.div`
   display: flex;
-  width: 1440px;
+  width: 1290px;
   margin-top: 150px;
-  justify-content: space-around;
+  gap: 20px;
+  justify-content: center;
+  flex-wrap: wrap;
+  @media(max-width: 1290px) {
+   width: 1000px;
+    flex-wrap:wrap;
+  }
+  @media(max-width: 900px) {
+   flex-direction: column;
+   width: 500px;
+   align-items: center;
+  }
   
 `;
