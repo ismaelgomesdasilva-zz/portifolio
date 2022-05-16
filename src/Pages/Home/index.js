eeca6b936cc75a6668d77d1b5d8fa7e2da2e164e
ismaelgomesdasilva <ismaelgomesdasilva003@gmail.com>
2022-05-16 19:37:47 -0300

    fix: card home

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,6 +1,8 @@
 import { MdWavingHand } from "react-icons/md";
 import avatar from "../../Assets/avatar.svg";
 import card1 from "../../Assets/card1.png";
+import card2 from "../../Assets/card2.png";
+import card3 from "../../Assets/card3.png";
 import { Container, Award, AwardDiv } from "./styles";
 export default function Home() {
   return (
@@ -21,14 +23,14 @@ export default function Home() {
           </div>
         </Award>
         <Award>
-          <img src={card1} />
+          <img src={card2} />
 
           <div className="description">
             <p>5+</p> <h2>Meses de experiencia</h2>
           </div>
         </Award>
         <Award>
-          <img src={card1} />
+          <img src={card3} />
 
           <div className="description">
             <p>5+</p> <h2>Meses de experiencia</h2>
